587a1cb8f233ac3611c5975f5827b96c2d8ef4b3
Safi <139801311+safi-siddiqui-github@users.noreply.github.com>
2023-12-14 16:59:57 +0500

    start

---
+++ models/role.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const roleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+}, {timestamps: true});
+
+export const Role = mongoose.model("Role", roleSchema);
---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roleType: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+    },
+}, {timestamps: true});
+
+export const User = mongoose.model("User", userSchema);
